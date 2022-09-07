4be996099d4654154eeed2228c2d0a9e674bff93
David Cimadom <david.cimadom@zone35.de>
2022-09-07 14:02:04 +0200

    initial

---
+++ qtile/scripts/autostart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export PATH="/home/davidc/.local/bin:$PATH"
+/usr/lib/xfce-polkit/xfce-polkit &
+bg.sh &
+l.sh &
+picom -b &
+eww daemon &
+mkfifo /tmp/vol-icon && ~/.config/qtile/scripts/vol_icon.sh &
+dunst &
+snixembed --fork &
+iwgtk -i &
+flameshot &
+tp-battery-icon &
+networkd-notify &
+protonmail-bridge &
+kitty --listen-on=unix:@neomutt --class="coms" -e "neomutt" &
+kitty --listen-on=unix:@newsboat --class="coms" -e "newsboat" &
+kitty --listen-on=unix:@ikhal --class="coms" -e "ikhal" &
+kitty --listen-on=unix:@btop --class="coms" -e "btop" &
+kitty --listen-on=unix:@zabbix --class="mon" -e "zabbixmon" &
+kitty --listen-on=unix:@spotify --class="Spotify" -e "spotify_player" &
+kitty --listen-on=unix:@neovim --class="neovim" -e "nvim" &
+kitty --listen-on=unix:@term --class="term" &
+qutebrowser &
