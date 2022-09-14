81e2bff98806eb75591335e213aa1061ece79e39
David Cimadom <david.cimadom@zone35.de>
2022-09-14 19:23:12 +0200

    testing

--- default_config.py
+++ default_config.py
@@ -29,7 +29,7 @@ bring_front_click = False
 cursor_warp = False
 auto_fullscreen = True
 focus_on_window_activation = "smart"
-# reconfigure_screens = True
+reconfigure_screens = True
 auto_minimize = False
 wmname = "qtile"
 
