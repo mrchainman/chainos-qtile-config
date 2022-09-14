f5056844d842288ce28f3c8db4b698b0e5519e58
David Cimadom <david.cimadom@zone35.de>
2022-09-14 19:10:01 +0200

    testing

--- default_config.py
+++ default_config.py
@@ -10,25 +10,25 @@ from libqtile.resources.modules.screens import screens, main_screen_bar, widget_
 # TODOs
 # - customize special rofi menus - DONE (room for improvement)
 # - qtile hooks
-
+mouse = []
 # REVIEW options
-dgroups_key_binder = None
-dgroups_app_rules = []  # type: list
-follow_mouse_focus = True
-bring_front_click = False
-cursor_warp = False
-auto_fullscreen = True
-focus_on_window_activation = "smart"
-# reconfigure_screens = True
-auto_minimize = False
-wmname = "qtile"
+# dgroups_key_binder = None
+# dgroups_app_rules = []  # type: list
+# follow_mouse_focus = True
+# bring_front_click = False
+# cursor_warp = False
+# auto_fullscreen = True
+# focus_on_window_activation = "smart"
+# # reconfigure_screens = True
+# auto_minimize = False
+# wmname = "qtile"
 
-# TODO take a look at hooks
-# @hook.subscribe.startup
-# def start():
-#     main_screen_bar.window.window.set_property("QTILE_BAR", 1)
+# # TODO take a look at hooks
+# # @hook.subscribe.startup
+# # def start():
+# #     main_screen_bar.window.window.set_property("QTILE_BAR", 1)
 
-@hook.subscribe.startup_once
-def start_once():
-    home = os.path.expanduser("~")
-    subprocess.call([home + ".config/chainos/qtile_autostart.sh"])
+# @hook.subscribe.startup_once
+# def start_once():
+#     home = os.path.expanduser("~")
+#     subprocess.call([home + ".config/chainos/qtile_autostart.sh"])
