344db07f10d1b1be73aad56cba9d3c4264b67456
Mr Chainman <mrchainman@chainman.xyz>
2023-04-24 15:37:26 +0200

    Changed clock backend

--- modules/popups/clock.py
+++ modules/popups/clock.py
@@ -2,8 +2,11 @@ from libqtile.lazy import lazy
 from libqtile.backend.x11.window import Window
 from qtile_extras.popup.toolkit import (
     PopupText,
-    PopupRelativeLayout
+    PopupWidget,
+    PopupRelativeLayout,
+    PopupAbsoluteLayout
 )
+from libqtile import widget
 from datetime import date, datetime
 import time
 from libqtile.resources.utils.settings import colors
@@ -11,19 +14,31 @@ from libqtile.resources.utils.settings import colors
 
 def gen_clock(qtile):
     controls = []
-    cur_time = datetime.now().strftime("%H:%M:%S")
+    # cur_time = datetime.now().strftime("%H:%M:%S")
+    # controls.append(
+    #         PopupText(
+    #              text=str(cur_time),
+    #              width=1,
+    #              height=1,
+    #              h_align="center",
+    #              highlight=colors["accent"],
+    #              highlight_method="block",
+    #              background=colors["trans"],               
+    #              name="clock",
+    #             )
+    #         )
     controls.append(
-            PopupText(
-                 text=str(cur_time),
-                 width=1,
-                 height=1,
-                 h_align="center",
-                 highlight=colors["accent"],
-                 highlight_method="block",
-                 background=colors["trans"],               
+            PopupWidget(
+                widget = widget.Clock(
+                    foreground=colors["accent"],
+                    format='%H:%M:%S',
+                    ),
+                name = "clock",
                 )
             )
-    layout = PopupRelativeLayout(
+    global layout
+    # layout = PopupRelativeLayout(
+    layout = PopupAbsoluteLayout(
             qtile,
             width=300,
             height=50,
@@ -32,3 +47,6 @@ def gen_clock(qtile):
             initial_focus=None,
             )
     layout.show(x=0, y=0, relative_to = 3, relative_to_bar=True)
+
+def update_clock(qtile):
+    pass
