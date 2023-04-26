f6f9b520ea8024725e0e688b20f6c1e36643c64f
Mr Chainman <mrchainman@chainman.xyz>
2023-04-26 15:40:11 +0200

    prettyfy prompt

--- executables/prompt.py
+++ executables/prompt.py
@@ -2,11 +2,14 @@
 from libqtile.resources.utils.settings import colors
 import sys
 class Prompt():
-    def __init__(self,color: str = "#efefef", color2: str = "#bebbeb", prompt_style: str = "default", plugins: list = ["line","prompt"]):
+    def __init__(self,color: str = "#efefef", color2: str = "#bebbeb", prompt_style: str = "default", plugins: list = [[]], show_line: bool = False, show_line_head: bool = False, show_prompt: bool = True ):
         self.color = color
         self.color2 = color2
         self.prompt_style = prompt_style
         self.plugins = plugins
+        self.show_line = show_line
+        self.show_line_head = show_line_head
+        self.show_prompt = show_prompt
         self.rgb = self.hex2rgb(color)
         self.rgb2 = self.hex2rgb(color2)
 
@@ -15,65 +18,81 @@ class Prompt():
         return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
         
 
-    def create_colors(self):
+    def construct_prompt(self):
         c_r1,c_g1,c_b1 = self.rgb
         self.p_color1 = f"\[\033[38;2;{c_r1};{c_g1};{c_b1}m\["
         c_r2,c_g2,c_b2 = self.rgb2
         self.p_color2 = f"\[\033[38;2;{c_r2};{c_g2};{c_b2}m\["
 
-    def create_utils(self):
-        self.line = self.p_color2 + "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n" if "line" in self.plugins else ""
-        self.ps1spacerformat = "┣━━ "
-        self.ps1spacer = self.p_color2 + self.ps1spacerformat
-        self.ps2spacer = ''.join([" " for i in self.ps1spacerformat])
+        self.line_head = self.p_color2 + "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n"
+        self.line = self.p_color2 + "┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫\n"
+        self.spacerbeginning = "┣━┫ "
+        self.spacerend = " ┣━┫"
+        self.spacermiddle = " ┣━┫ "
+        self.spacerblank = ''.join([" " for i in self.spacermiddle])
+        self.individual_spacer = " ┣━━━━━━━━━━┫"
+        self.newline = "\n"
 
-    def construct_prompt(self):
-        """
-        Unicodes:
-━
+        self.prompt = self.p_color2 + "┗━━━┫ "
+        self.follower = self.p_color2 + self.spacerblank + "┗━━━┫ "
 
-┏
-
-┃
-
-┓
-
-┗
-
-w
-
-┛
-
-┣
-
-┫
-
-┯
-
-┻
+        self.git = "Git Branch: $(git branch 2>/dev/null | grep '^\*' | awk '{print $NF}'  || '')"
+        self.cwd = "Current Direcory: \W"
+        self.exitcode = "Exitcode: $?"
+        self.packages = "Packages: $(pacman -Q | wc -l)"
+        self.amiroot = "User: $(whoami)"
 
 
 
-        """
-        self.git = self.ps1spacer + self.p_color1 + "Git Branch: $(git branch 2>/dev/null | grep '^\*' | awk '{print $NF}'  || '') \n" if "git" in self.plugins else ""
-        self.cwd = self.ps1spacer + self.p_color1 + "Current Direcory: \W\n" if "cwd" in self.plugins else ""
-        self.exitcode = self.ps1spacer + self.p_color1 + "Exitcode: $?\n" if "exitcode" in self.plugins else ""
-        self.prompt = self.p_color2 + "┗━━━┫ " if "prompt" in self.plugins else ""
-        print(f"{self.line}{self.cwd}{self.git}{self.exitcode}{self.prompt}{self.p_color1}")
+    def generate_complete_prompt(self):
+        self.construct_prompt()
+        self.full_prompt = []
+        for lst in self.plugins:
+            self.full_prompt.append(self.p_color2)
+            if self.show_line:
+                self.full_prompt.append(self.line)
+            for i in lst:
+                self.full_prompt.append(self.p_color2)
+                if lst.index(i) == 0:
+                    self.full_prompt.append(self.spacerbeginning)
+                else:
+                    self.full_prompt.append(self.spacermiddle)
+                self.full_prompt.append(self.p_color1)
+                self.full_prompt.append(getattr(self,i))
+            # self.full_prompt.append(self.spacerend)
+            self.full_prompt.append(self.newline)
 
-    def construct_follower(self):
-        self.follower = "┗━━━┫ "
-        print(f"{self.p_color2}{self.ps2spacer}{self.follower}{self.p_color1}")
+        if self.show_line_head:
+            self.full_prompt.insert(0, self.line_head)
+        if self.show_prompt:
+            self.full_prompt.append(self.prompt)
 
 
-#283141
-ps1 = Prompt(color=colors["accent"],color2=colors["accent2"],plugins = ["line","prompt","git","cwd","exitcode"])
-ps1.create_colors()
-ps1.create_utils()
+    def show_ps1(self):
+        self.generate_complete_prompt()
+        print(''.join(self.full_prompt))
+
+    def show_follower(self):
+        self.generate_complete_prompt()
+        print('')
+
+
+ps1 = Prompt(
+        color=colors["accent"],
+        color2=colors["accent2"],
+        plugins = [
+            ["cwd", "git"],
+            ["amiroot", "exitcode","packages"],
+            ],
+        show_line = True,
+        show_line_head = True,
+        show_prompt = True,
+        )
+
 if len(sys.argv) > 1:
     if sys.argv[1] == "ps2":
-        ps1.construct_follower()
+        ps1.show_follower()
 else:
-    ps1.construct_prompt()
+    ps1.show_ps1()
 
 
