8584fedb389a6310ae7a5a69074ea7b9a92b0e10
Mr Chainman <mrchainman@chainman.xyz>
2023-04-26 14:10:18 +0200

    prettify

--- executables/prompt.py
+++ executables/prompt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from libqtile.resources.utils.settings import colors
+import sys
 class Prompt():
     def __init__(self,color: str = "#efefef", color2: str = "#bebbeb", prompt_style: str = "default", plugins: list = ["line","prompt"]):
         self.color = color
@@ -13,6 +14,18 @@ class Prompt():
         hex = col.lstrip('#')
         return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
         
+
+    def create_colors(self):
+        c_r1,c_g1,c_b1 = self.rgb
+        self.p_color1 = f"\[\033[38;2;{c_r1};{c_g1};{c_b1}m\["
+        c_r2,c_g2,c_b2 = self.rgb2
+        self.p_color2 = f"\[\033[38;2;{c_r2};{c_g2};{c_b2}m\["
+
+    def create_utils(self):
+        self.line = self.p_color2 + "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n" if "line" in self.plugins else ""
+        self.ps1spacerformat = "┣━━ "
+        self.ps1spacer = self.p_color2 + self.ps1spacerformat
+        self.ps2spacer = ''.join([" " for i in self.ps1spacerformat])
 
     def construct_prompt(self):
         """
@@ -42,20 +55,25 @@ w
 
 
         """
-        c_r1,c_g1,c_b1 = self.rgb
-        color1 = f"\[\033[38;2;{c_r1};{c_g1};{c_b1}m\["
-        c_r2,c_g2,c_b2 = self.rgb2
-        color2 = f"\[\033[38;2;{c_r2};{c_g2};{c_b2}m\["
-        line = color2 + "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n" if "line" in self.plugins else ""
-        spacer = color2 + "┣━━ "
-        git = spacer + color1 + "Git Branch: $(git branch 2>/dev/null | grep '^\*' | awk '{print $NF}'  || '') \n" if "git" in self.plugins else ""
-        cwd = spacer + color1 + "Current Direcory: \W\n" if "cwd" in self.plugins else ""
-        exitcode = spacer + color1 + "Exitcode: $?\n" if "exitcode" in self.plugins else ""
-        prompt = color2 + "┗━━━┫ " if "prompt" in self.plugins else ""
-        print(f"{line}{cwd}{git}{exitcode}{prompt}{color1}")
+        self.git = self.ps1spacer + self.p_color1 + "Git Branch: $(git branch 2>/dev/null | grep '^\*' | awk '{print $NF}'  || '') \n" if "git" in self.plugins else ""
+        self.cwd = self.ps1spacer + self.p_color1 + "Current Direcory: \W\n" if "cwd" in self.plugins else ""
+        self.exitcode = self.ps1spacer + self.p_color1 + "Exitcode: $?\n" if "exitcode" in self.plugins else ""
+        self.prompt = self.p_color2 + "┗━━━┫ " if "prompt" in self.plugins else ""
+        print(f"{self.line}{self.cwd}{self.git}{self.exitcode}{self.prompt}{self.p_color1}")
+
+    def construct_follower(self):
+        self.follower = "┗━━━┫ "
+        print(f"{self.p_color2}{self.ps2spacer}{self.follower}{self.p_color1}")
+
 
 #283141
 ps1 = Prompt(color=colors["accent"],color2=colors["accent2"],plugins = ["line","prompt","git","cwd","exitcode"])
-ps1.construct_prompt()
+ps1.create_colors()
+ps1.create_utils()
+if len(sys.argv) > 1:
+    if sys.argv[1] == "ps2":
+        ps1.construct_follower()
+else:
+    ps1.construct_prompt()
 
 
