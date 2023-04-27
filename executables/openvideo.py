e1d106d546a2654262210dd4fe33d4deb45ed48c
Mr Chainman <mrchainman@chainman.xyz>
2023-04-27 18:28:17 +0200

    Typehinting and docs

--- executables/openvideo.py
+++ executables/openvideo.py
@@ -10,4 +10,3 @@ if __name__ == "__main__":
     mpv_run(url = sys.argv[1])
 
 
-
--- executables/prompt.py
+++ executables/prompt.py
@@ -2,6 +2,9 @@
 from libqtile.resources.utils.settings import colors
 import sys
 class Prompt():
+    """
+    Defines the full Bashprompt
+    """
     def __init__(self,color: str = "#efefef", color2: str = "#bebbeb", prompt_style: str = "default", plugins: list = [[]], show_line: bool = False, show_line_head: bool = False, show_prompt: bool = True ):
         self.color = color
         self.color2 = color2
@@ -13,12 +16,19 @@ class Prompt():
         self.rgb = self.hex2rgb(color)
         self.rgb2 = self.hex2rgb(color2)
 
-    def hex2rgb(self,col) -> tuple:
+    def hex2rgb(self, col: str) -> tuple:
+        """
+        Convert a hexcolorstring to rbg.
+        """
         hex = col.lstrip('#')
-        return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
+        rgbs = tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
+        return rgbs
         
 
     def construct_prompt(self):
+        """
+        Construct all the parts used in the prompt. 
+        """
         c_r1,c_g1,c_b1 = self.rgb
         self.p_color1 = f"\[\033[38;2;{c_r1};{c_g1};{c_b1}m\["
         c_r2,c_g2,c_b2 = self.rgb2
@@ -32,27 +42,27 @@ class Prompt():
         self.spacerend = " ┣━┫"
         self.spacermiddle = " ┣━┫ "
         self.spacerblank = ''.join([" " for i in self.spacermiddle])
-        self.individual_spacer = " ┣━━━━━━━━━━┫"
         self.newline = "\n"
 
         self.prompt = self.p_color2 + "┗━━━┫ "
         self.follower = self.p_color2 + self.spacerblank + "┗━━━┫ "
 
-        self.git = "Git Branch: $(git branch 2>/dev/null | grep '^\*' | awk '{print $NF}'  || '')"
+        self.git = "Git Branch: $(git branch 2>/dev/null | grep '^\*' | awk '{print $NF}') Modified Files: $(git status --short 2>/dev/null | wc -l) Last Commit: $(git show --oneline 2>/dev/null | head -n 1)"
         self.cwd = "Current Direcory: \W"
         self.exitcode = "Exitcode: $?"
         self.packages = "Packages: $(pacman -Q | wc -l)"
         self.amiroot = "User: $(whoami)"
+        self.updates = "Updates: $(checkupdates | wc -l)"
 
 
 
-    def generate_complete_prompt(self):
+    def generate_complete_prompt(self) -> list:
+        """
+        Generates a list containing all the parts used in the prompt.
+        """
         self.construct_prompt()
         self.full_prompt = []
         for lst in self.plugins:
-            self.full_prompt.append(self.p_color2)
-            if self.show_line:
-                self.full_prompt.append(self.line)
             for i in lst:
                 self.full_prompt.append(self.p_color2)
                 if lst.index(i) == 0:
@@ -62,21 +72,34 @@ class Prompt():
                 self.full_prompt.append(self.p_color1)
                 self.full_prompt.append(getattr(self,i))
             # self.full_prompt.append(self.spacerend)
+            self.full_prompt.append(self.p_color2)
             self.full_prompt.append(self.newline)
+            if self.show_line:
+                self.full_prompt.append(self.line)
 
         if self.show_line_head:
             self.full_prompt.insert(0, self.line_head)
         if self.show_prompt:
             self.full_prompt.append(self.prompt)
+            self.full_prompt.append(self.p_color1)
+        return self.full_prompt
 
 
-    def show_ps1(self):
+    def show_ps1(self) -> str:
+        """
+        Converts the list of prompts to one single string for PS1
+        """
         self.generate_complete_prompt()
-        print(''.join(self.full_prompt))
+        string = ''.join(self.full_prompt)
+        return string
 
-    def show_follower(self):
+    def show_follower(self) -> str:
+        """
+        Converts the list of prompts to one single string for PS2
+        """
         self.generate_complete_prompt()
-        print('')
+        string = self.follower
+        return string
 
 
 ps1 = Prompt(
@@ -84,7 +107,7 @@ ps1 = Prompt(
         color2=colors["accent2"],
         plugins = [
             ["cwd", "git"],
-            ["amiroot", "exitcode","packages"],
+            ["amiroot", "exitcode","packages",],
             ],
         show_line = True,
         show_line_head = True,
@@ -93,8 +116,8 @@ ps1 = Prompt(
 
 if len(sys.argv) > 1:
     if sys.argv[1] == "ps2":
-        ps1.show_follower()
+        print(ps1.show_follower())
 else:
-    ps1.show_ps1()
+    print(ps1.show_ps1())
 
 
